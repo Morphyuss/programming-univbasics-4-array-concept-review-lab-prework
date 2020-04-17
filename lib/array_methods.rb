45d324ddcb9736d12cf92b0efda50551f8a2e78b
James Stamps Jr <jstampshere@gmail.com>
2020-04-17 02:06:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   counter = 0
   
   while counter < array.length do
-    array.length == value_to_find
+    array.length == value_to_find[index]
     counter += 1
     puts value_to_find
   end
