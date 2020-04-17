c06e8361472b6336aca488f9e1c356ae6e206c8c
James Stamps Jr <jstampshere@gmail.com>
2020-04-17 01:36:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,10 @@
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  
 end
+ 
+
+
 
 def find_max_value(array)
   # Add your solution here
@@ -9,3 +13,4 @@ end
 def find_min_value(array)
   # Add your solution here
 end
+
